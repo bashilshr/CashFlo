dec58e04bd326f0dc1b88e39610272e24b0f539f
bashilshr <bashil.shrestha@gmail.com>
2024-12-19 07:58:08 +0545

    Add project files.

---
+++ CashFlo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CashFlo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ CashFlo/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
